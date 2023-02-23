49fee4ccda923178b5f7d5c84304b43acfccafaf
thecodingmango <thecodingmango28@gmail.com>
2023-02-23 00:16:29 -0500

    Added Updator class that can fetch data from the BLS website - Added function that extract values from a list of dictionaries using specified key

--- data/data_update/fetch_data.py
+++ data/data_update/fetch_data.py
@@ -3,6 +3,15 @@
 # Importing libraries
 import requests
 import json
+from datetime import datetime
+import pandas as pd
+import numpy as np
+
+# Settings for global variables
+start_year = 2000
+end_year = datetime.now().year
+series = ['CUUR0000SA0', 'SUUR0000SA0']
+url_bls = 'https://api.bls.gov/publicAPI/v2/timeseries/data/'
 
 
 class Updator:
@@ -10,8 +19,8 @@ class Updator:
     def __init__(self, bls_series, bls_url, start_year, end_year):
         self.bls_series = bls_series
         self.bls_url = bls_url
-        self.start_year = str(start_year)
-        self.end_year = str(end_year)
+        self.start_year = start_year
+        self.end_year = end_year
 
     # Function to retrieve data from the US BLS website
     def retrieve_data_bls(self):
@@ -22,15 +31,43 @@ class Updator:
         """
 
         headers = {'Content-type': 'application/json'}
-        data = json.dumps({'seriesid': self.bls_series, 'startyear': self.start_year, 'endyear': self.end_year})
+        data = json.dumps({'seriesid': self.bls_series,
+                           'startyear': str(self.start_year),
+                           'endyear': str(self.end_year),
+                           "registrationkey": "064c315c02b84896aaa929d9a38510ac"})
 
         p = requests.post(self.bls_url, data=data, headers=headers)
         json_data = json.loads(p.text)
 
+        print(json_data)
+
         return json_data
 
+    # Function that converts dictionaries to list
+    def dict_to_list(self, keys, dicts):
 
-series = ['CUUR0000SA0', 'SUUR0000SA0']
-data = Updator(series, 'https://api.bls.gov/publicAPI/v1/timeseries/data/', 2010, 2020)
+        """
+        Function that takes in list of dictionaries and return the values in each of the dictionaries with the specified
+        key
+        :param keys: List of keys
+        :param dicts: List of Dict
+        :return: numpy array
+        """
+
+        list_array = []
+
+        for key in keys:
+
+            list_item = [item.get(key) for item in dicts]
+
+            list_array.append(list_item)
+
+        return np.array(list_array)
+
+
+
+data = Updator(series, url_bls, 2000, 2020)
 retrieved_data = data.retrieve_data_bls()
+retrieved_data_2 = data.retrieve_data_bls()
+data.dict_to_list(['year', 'periodName', 'value'], retrieved_data['Results']['series'][0]['data'])[2]
 
