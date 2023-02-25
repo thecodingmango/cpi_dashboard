70519c668639995bcc30113f774927ebb4e0a103
thecodingmango <thecodingmango28@gmail.com>
2023-02-25 17:32:14 -0500

    Added config files for the updator

---
+++ config.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+# Settings for global variables
+start_year = 2000
+end_year = datetime.now().year
+series = ['CUUR0000SA0', 'SUUR0000SA0']
+url_bls = 'https://api.bls.gov/publicAPI/v2/timeseries/data/'
