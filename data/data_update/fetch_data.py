2d69a90c6d0e2834441147927a90eb06c58f9dae
thecodingmango <thecodingmango28@gmail.com>
2023-02-25 17:31:37 -0500

    Added Updator class that can fetch data from the BLS website - Restructured Codes to better format

--- data/data_update/fetch_data.py
+++ data/data_update/fetch_data.py
@@ -3,24 +3,40 @@
 # Importing libraries
 import requests
 import json
-from datetime import datetime
 import pandas as pd
-import numpy as np
+import api_keys
+import config
 
-# Settings for global variables
-start_year = 2000
-end_year = datetime.now().year
-series = ['CUUR0000SA0', 'SUUR0000SA0']
-url_bls = 'https://api.bls.gov/publicAPI/v2/timeseries/data/'
+
+# Function that converts dictionaries to list
+def dict_to_list(keys, dicts):
+
+    """
+    Function that takes in list of dictionaries and return the values in each of the dictionaries with the specified
+    key
+    :param keys: List of keys
+    :param dicts: List of Dict
+    :return: numpy array
+    """
+
+    list_array = []
+
+    for key in keys:
+
+        list_item = [item.get(key) for item in dicts]
+
+        list_array.append(list_item)
+
+    return list_array
 
 
 class Updator:
 
-    def __init__(self, bls_series, bls_url, start_year, end_year):
-        self.bls_series = bls_series
-        self.bls_url = bls_url
-        self.start_year = start_year
-        self.end_year = end_year
+    def __init__(self):
+        self.bls_series = config.series
+        self.bls_url = config.url_bls
+        self.start_year = config.start_year
+        self.end_year = config.end_year
 
     # Function to retrieve data from the US BLS website
     def retrieve_data_bls(self):
@@ -34,7 +50,7 @@ class Updator:
         data = json.dumps({'seriesid': self.bls_series,
                            'startyear': str(self.start_year),
                            'endyear': str(self.end_year),
-                           "registrationkey": "064c315c02b84896aaa929d9a38510ac"})
+                           "registrationkey": api_keys.bls_api_key})
 
         p = requests.post(self.bls_url, data=data, headers=headers)
         json_data = json.loads(p.text)
@@ -43,31 +59,14 @@ class Updator:
 
         return json_data
 
-    # Function that converts dictionaries to list
-    def dict_to_list(self, keys, dicts):
 
-        """
-        Function that takes in list of dictionaries and return the values in each of the dictionaries with the specified
-        key
-        :param keys: List of keys
-        :param dicts: List of Dict
-        :return: numpy array
-        """
-
-        list_array = []
-
-        for key in keys:
-
-            list_item = [item.get(key) for item in dicts]
-
-            list_array.append(list_item)
-
-        return np.array(list_array)
-
-
-
-data = Updator(series, url_bls, 2000, 2020)
+data = Updator()
 retrieved_data = data.retrieve_data_bls()
 retrieved_data_2 = data.retrieve_data_bls()
-data.dict_to_list(['year', 'periodName', 'value'], retrieved_data['Results']['series'][0]['data'])[2]
+test = dict_to_list(['year', 'periodName', 'value'], retrieved_data['Results']['series'][1]['data'])
 
+test = []
+for series in retrieved_data['Results']['series']:
+
+    test += dict_to_list(['value'], series['data'])
+pd.DataFrame(test).transpose()
