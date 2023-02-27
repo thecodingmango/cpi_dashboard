4eefddb8b564a94249c7ac08367fd1a60925fd72
thecodingmango <thecodingmango28@gmail.com>
2023-02-26 23:16:12 -0500

    Revert "Revert "Added more series to get from the API""
    This reverts commit 46dd06348dfc2b32c7e491fd0e157c9094a3f011.

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 from datetime import datetime
 
 # Settings for global variables
-start_year = 2000
+start_year = datetime.now().year - 19
 end_year = datetime.now().year
-series = ['CUUR0000SA0', 'SUUR0000SA0']
+bls_series = ['APU0000708111', 'APU000072610', 'APU0000709112', 'APU0000702111', 'APU0000704111', 'APU0000FF1101',
+              'APU0000706111', 'APU0000711211', 'APU0000701312', 'APU0000717311', 'APU0000703111', 'APU0000702421',
+              'APU0000711311', 'APU0000712311', 'CUUR0000SA0']
+bls_series_name = ['eggs', 'electricity', 'milk', 'bread', 'bacon', 'chicken_breast', 'chicken_whole', 'bananas',
+                   'rice', 'coffee', 'ground_chuck', 'cookies', 'oranges', 'tomatoes', 'cpi_values']
 url_bls = 'https://api.bls.gov/publicAPI/v2/timeseries/data/'
+
+
