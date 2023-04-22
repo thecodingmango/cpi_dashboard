d86d84df5d08f573df5f3f4cbf10aa1375979697
thecodingmango <thecodingmango28@gmail.com>
2023-04-22 16:48:39 -0400

    - Updated the style.css

--- cpi_dashboard.py
+++ cpi_dashboard.py
@@ -30,217 +30,239 @@ external_stylesheets = [
 # Initialize the dash class
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title = 'Crude Oil to CPI Analysis'
+app.title = 'CPI Dashboard'
 
 app.layout = html.Div(
     children=[
         html.Div(
             children=[
                 html.H1(
-                    children='CPI Analysis',
+                    children='CPI DashBoard',
                     className='header-title'
                 ),
-                html.P(children='Dash board for visualizing CPI data '
-                                ' compared to oil prices',
+                html.P(children='Dashboard to Visualize CPI and Different Commodity',
                        className='header-description')
             ],
-            className='header',
-        ),
-        html.Div(
-            children=[
-                html.Div(
-                    children='Date',
-                    className='menu-title'),
-                dcc.DatePickerRange(
-                    id='date_range',
-                    min_date_allowed=all_data.DATE.min(),
-                    max_date_allowed=all_data.DATE.max(),
-                    start_date=all_data.DATE.min(),
-                    end_date=all_data.DATE.max()
-                )
-            ],
-            className='menu'
-        ),
-        html.Div(
-            children=dcc.Graph(
-                id='cpi_chart',
-                config={'displayModeBar': True}
-
-            ), style={'display': 'inline-block'}, className='card'
-        ),
-        html.Div(
-            children=[
-                html.Div(
-                    children=dcc.Graph(
-                        id='fuel_price',
-                        config={'displayModeBar': True}
-                    ),
-                    style={'display': 'inline-block'}, className='card'
-                ),
-                html.Div(
-                    children=dcc.Graph(
-                        id='crude_price',
-                        config={'displayModeBar': True}
-                    ),
-                    style={'display': 'inline-block'}, className='card'
-                )
-            ],
-        ),
-        html.Div(
-            children=[
-                html.Div(
-                    children=dcc.Graph(
-                        id='production',
-                        config={'displayModeBar': True}
-
-                    ), style={'display': 'inline-block'}, className='card'
-                ),
-                html.Div(
-                    children=dcc.Graph(
-                        id='consumption',
-                        config={'displayModeBar': True}
-                    ),
-                    style={'display': 'inline-block'}, className='card'
-                )
-            ]
-        ),
-        html.Div(
-            children=dcc.Graph(
-                id='production_consumption',
-                config={'displayModeBar': True}
-            ), className='card'
+            className='header'
         )
     ]
 )
 
 
-@app.callback(
-    [Output('cpi_chart', 'figure'),
-     Output('fuel_price', 'figure'),
-     Output('crude_price', 'figure'),
-     Output('production', 'figure'),
-     Output('consumption', 'figure'),
-     Output('production_consumption', 'figure')],
-    [Input('date_range', 'start_date'),
-     Input('date_range', 'end_date')]
-)
-def update_chart(start_date, end_date):
-    filters = ((all_data.DATE >= start_date) & (all_data.DATE <= end_date))
-    filtered_data = all_data.loc[filters, :]
 
-    cpi_chart = {
-        'data': [
-            {
-                'x': filtered_data['DATE'],
-                'y': filtered_data['VALUE'],
-                'type': 'lines'
-            }
-        ],
-        'layout': {
-            'title':
-                {'text': 'CPI Values Since 1997',
-                 'x': 0.05},
-            'xaxis': {'fixed_range': True},
-            'yaxis': {'ticksuffix': '%', 'fixed_range': True},
-            'width': 1900
-        }
-    }
 
-    fuel_price = {
-        'data': [
-            {'x': filtered_data['DATE'], 'y': filtered_data['Gasoline_Price'], 'type': 'lines',
-             'name': 'Gasoline Price'},
-            {'x': filtered_data['DATE'], 'y': filtered_data['Diesel_Fuel_Price'], 'type': 'lines',
-             'name': 'Diesel Price'},
-            {'x': filtered_data['DATE'], 'y': filtered_data['Fuel_Oil_Price'], 'type': 'lines', 'name': 'Fuel Price'}
-        ],
-        'layout': {
-            'title':
-                {'text': 'Gasoline, Diesel, Fuel Prices Since 1997',
-                 'x': 0.05},
-            'xaxis': {'fixed_range': True},
-            'yaxis': {'tickprefix': '$', 'fixed_range': True},
-            'width': 950
-        }
-    }
 
-    crude_price = {
-        'data': [
-            {'x': filtered_data['DATE'], 'y': filtered_data['West_Texas_Crude_Oil_Spot_Price'], 'type': 'lines',
-             'name': 'West Texas Crude Price'},
-            {'x': filtered_data['DATE'], 'y': filtered_data['Brent_Crude_Oil_Spot_Price'], 'type': 'lines',
-             'name': 'Brent Crude Price'},
-            {'x': filtered_data['DATE'], 'y': filtered_data['Imported_Crude_Oil_Average_Price'], 'type': 'lines',
-             'name': 'Imported Crude Price'}
-        ],
-        'layout': {
-            'title':
-                {'text': 'Comparison of Different Crude Oil Prices',
-                 'x': 0.05},
-            'xaxis': {'fixed_range': True},
-            'yaxis': {'tickprefix': '$', 'fixed_range': True},
-            'width': 950
-        }
-    }
-    production = {
-        'data': [
-            {'x': filtered_data['DATE'], 'y': filtered_data['US_Production'], 'type': 'lines',
-             'name': 'US Oil Production'},
-            {'x': filtered_data['DATE'], 'y': filtered_data['Canada_Production'], 'type': 'lines',
-             'name': 'Canada Oil Production'},
-            {'x': filtered_data['DATE'], 'y': filtered_data['Mexico_Production'], 'type': 'lines',
-             'name': 'Mexico Oil Production'},
-            {'x': filtered_data['DATE'], 'y': filtered_data['Eurasia_Production'], 'type': 'lines',
-             'name': 'Eurasia Oil Production'}
-        ],
-        'layout': {
-            'title':
-                {'text': 'Comparison of Different Crude Oil Prices',
-                 'x': 0.05},
-            'xaxis': {'fixed_range': True},
-            'yaxis': {'ticksuffix': ' Million Barrels', 'fixed_range': True},
-            'margin': {'l': 125},
-            'width': 950
-        }
-    }
-    consumption = {
-        'data': [
-            {'x': filtered_data['DATE'], 'y': filtered_data['US_Consumption'], 'type': 'lines',
-             'name': 'US Oil Consumption'},
-            {'x': filtered_data['DATE'], 'y': filtered_data['Canada_Consumption'], 'type': 'lines',
-             'name': 'Canada Oil Consumption'},
-            {'x': filtered_data['DATE'], 'y': filtered_data['Europe_Consumption'], 'type': 'lines',
-             'name': 'Mexico Oil Consumption'},
-        ],
-        'layout': {
-            'title':
-                {'text': 'Comparison of Different Crude Oil Prices',
-                 'x': 0.05},
-            'xaxis': {'fixed_range': True},
-            'yaxis': {'ticksuffix': ' Million Barrels', 'fixed_range': True},
-            'margin': {'l': 125},
-            'width': 950
-        }
-    }
-    production_consumption = {
-        'data': [
-            {'x': filtered_data['DATE'], 'y': filtered_data['Total_World_Consumption'], 'type': 'lines',
-             'name': 'Total World Consumption/Month'},
-            {'x': filtered_data['DATE'], 'y': filtered_data['Total_World_Production'], 'type': 'lines',
-             'name': 'Total World Production/Month'},
-        ],
-        'layout': {
-            'title':
-                {'text': 'Comparison of Total World Oil Production and Total World Oil Consumption',
-                 'x': 0.05},
-            'xaxis': {'fixed_range': True},
-            'yaxis': {'ticksuffix': ' Million Barrels', 'fixed_range': True},
-            'margin': {'l': 125}
-        }
-    }
 
-    return [cpi_chart, fuel_price, crude_price, production, consumption, production_consumption]
+
+# app.layout = html.Div(
+#     children=[
+#         html.Div(
+#             children=[
+#                 html.H1(
+#                     children='CPI Analysis',
+#                     className='header-title'
+#                 ),
+#                 html.P(children='Dash board for visualizing CPI data '
+#                                 ' compared to oil prices',
+#                        className='header-description')
+#             ],
+#             className='header',
+#         ),
+#         html.Div(
+#             children=[
+#                 html.Div(
+#                     children='Date',
+#                     className='menu-title'),
+#                 dcc.DatePickerRange(
+#                     id='date_range',
+#                     min_date_allowed=all_data.DATE.min(),
+#                     max_date_allowed=all_data.DATE.max(),
+#                     start_date=all_data.DATE.min(),
+#                     end_date=all_data.DATE.max()
+#                 )
+#             ],
+#             className='menu'
+#         ),
+#         html.Div(
+#             children=dcc.Graph(
+#                 id='cpi_chart',
+#                 config={'displayModeBar': True}
+#
+#             ), style={'display': 'inline-block'}, className='card'
+#         ),
+#         html.Div(
+#             children=[
+#                 html.Div(
+#                     children=dcc.Graph(
+#                         id='fuel_price',
+#                         config={'displayModeBar': True}
+#                     ),
+#                     style={'display': 'inline-block'}, className='card'
+#                 ),
+#                 html.Div(
+#                     children=dcc.Graph(
+#                         id='crude_price',
+#                         config={'displayModeBar': True}
+#                     ),
+#                     style={'display': 'inline-block'}, className='card'
+#                 )
+#             ],
+#         ),
+#         html.Div(
+#             children=[
+#                 html.Div(
+#                     children=dcc.Graph(
+#                         id='production',
+#                         config={'displayModeBar': True}
+#
+#                     ), style={'display': 'inline-block'}, className='card'
+#                 ),
+#                 html.Div(
+#                     children=dcc.Graph(
+#                         id='consumption',
+#                         config={'displayModeBar': True}
+#                     ),
+#                     style={'display': 'inline-block'}, className='card'
+#                 )
+#             ]
+#         ),
+#         html.Div(
+#             children=dcc.Graph(
+#                 id='production_consumption',
+#                 config={'displayModeBar': True}
+#             ), className='card'
+#         )
+#     ]
+# )
+#
+#
+# @app.callback(
+#     [Output('cpi_chart', 'figure'),
+#      Output('fuel_price', 'figure'),
+#      Output('crude_price', 'figure'),
+#      Output('production', 'figure'),
+#      Output('consumption', 'figure'),
+#      Output('production_consumption', 'figure')],
+#     [Input('date_range', 'start_date'),
+#      Input('date_range', 'end_date')]
+# )
+# def update_chart(start_date, end_date):
+#     filters = ((all_data.DATE >= start_date) & (all_data.DATE <= end_date))
+#     filtered_data = all_data.loc[filters, :]
+#
+#     cpi_chart = {
+#         'data': [
+#             {
+#                 'x': filtered_data['DATE'],
+#                 'y': filtered_data['VALUE'],
+#                 'type': 'lines'
+#             }
+#         ],
+#         'layout': {
+#             'title':
+#                 {'text': 'CPI Values Since 1997',
+#                  'x': 0.05},
+#             'xaxis': {'fixed_range': True},
+#             'yaxis': {'ticksuffix': '%', 'fixed_range': True},
+#             'width': 1900
+#         }
+#     }
+#
+#     fuel_price = {
+#         'data': [
+#             {'x': filtered_data['DATE'], 'y': filtered_data['Gasoline_Price'], 'type': 'lines',
+#              'name': 'Gasoline Price'},
+#             {'x': filtered_data['DATE'], 'y': filtered_data['Diesel_Fuel_Price'], 'type': 'lines',
+#              'name': 'Diesel Price'},
+#             {'x': filtered_data['DATE'], 'y': filtered_data['Fuel_Oil_Price'], 'type': 'lines', 'name': 'Fuel Price'}
+#         ],
+#         'layout': {
+#             'title':
+#                 {'text': 'Gasoline, Diesel, Fuel Prices Since 1997',
+#                  'x': 0.05},
+#             'xaxis': {'fixed_range': True},
+#             'yaxis': {'tickprefix': '$', 'fixed_range': True},
+#             'width': 950
+#         }
+#     }
+#
+#     crude_price = {
+#         'data': [
+#             {'x': filtered_data['DATE'], 'y': filtered_data['West_Texas_Crude_Oil_Spot_Price'], 'type': 'lines',
+#              'name': 'West Texas Crude Price'},
+#             {'x': filtered_data['DATE'], 'y': filtered_data['Brent_Crude_Oil_Spot_Price'], 'type': 'lines',
+#              'name': 'Brent Crude Price'},
+#             {'x': filtered_data['DATE'], 'y': filtered_data['Imported_Crude_Oil_Average_Price'], 'type': 'lines',
+#              'name': 'Imported Crude Price'}
+#         ],
+#         'layout': {
+#             'title':
+#                 {'text': 'Comparison of Different Crude Oil Prices',
+#                  'x': 0.05},
+#             'xaxis': {'fixed_range': True},
+#             'yaxis': {'tickprefix': '$', 'fixed_range': True},
+#             'width': 950
+#         }
+#     }
+#     production = {
+#         'data': [
+#             {'x': filtered_data['DATE'], 'y': filtered_data['US_Production'], 'type': 'lines',
+#              'name': 'US Oil Production'},
+#             {'x': filtered_data['DATE'], 'y': filtered_data['Canada_Production'], 'type': 'lines',
+#              'name': 'Canada Oil Production'},
+#             {'x': filtered_data['DATE'], 'y': filtered_data['Mexico_Production'], 'type': 'lines',
+#              'name': 'Mexico Oil Production'},
+#             {'x': filtered_data['DATE'], 'y': filtered_data['Eurasia_Production'], 'type': 'lines',
+#              'name': 'Eurasia Oil Production'}
+#         ],
+#         'layout': {
+#             'title':
+#                 {'text': 'Comparison of Different Crude Oil Prices',
+#                  'x': 0.05},
+#             'xaxis': {'fixed_range': True},
+#             'yaxis': {'ticksuffix': ' Million Barrels', 'fixed_range': True},
+#             'margin': {'l': 125},
+#             'width': 950
+#         }
+#     }
+#     consumption = {
+#         'data': [
+#             {'x': filtered_data['DATE'], 'y': filtered_data['US_Consumption'], 'type': 'lines',
+#              'name': 'US Oil Consumption'},
+#             {'x': filtered_data['DATE'], 'y': filtered_data['Canada_Consumption'], 'type': 'lines',
+#              'name': 'Canada Oil Consumption'},
+#             {'x': filtered_data['DATE'], 'y': filtered_data['Europe_Consumption'], 'type': 'lines',
+#              'name': 'Mexico Oil Consumption'},
+#         ],
+#         'layout': {
+#             'title':
+#                 {'text': 'Comparison of Different Crude Oil Prices',
+#                  'x': 0.05},
+#             'xaxis': {'fixed_range': True},
+#             'yaxis': {'ticksuffix': ' Million Barrels', 'fixed_range': True},
+#             'margin': {'l': 125},
+#             'width': 950
+#         }
+#     }
+#     production_consumption = {
+#         'data': [
+#             {'x': filtered_data['DATE'], 'y': filtered_data['Total_World_Consumption'], 'type': 'lines',
+#              'name': 'Total World Consumption/Month'},
+#             {'x': filtered_data['DATE'], 'y': filtered_data['Total_World_Production'], 'type': 'lines',
+#              'name': 'Total World Production/Month'},
+#         ],
+#         'layout': {
+#             'title':
+#                 {'text': 'Comparison of Total World Oil Production and Total World Oil Consumption',
+#                  'x': 0.05},
+#             'xaxis': {'fixed_range': True},
+#             'yaxis': {'ticksuffix': ' Million Barrels', 'fixed_range': True},
+#             'margin': {'l': 125}
+#         }
+#     }
+#
+#     return [cpi_chart, fuel_price, crude_price, production, consumption, production_consumption]
 
 
 if __name__ == "__main__":
-    app.run_server()
+    app.run_server(debug=True)
