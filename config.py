d718bb795e6963c521d2eb973580452823b59c0b
thecodingmango <thecodingmango28@gmail.com>
2023-07-08 17:40:19 -0400

    - Separated some functions from the Updater class for better readability

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ bls_series = ['APU0000708111', 'APU000072610', 'APU0000709112', 'APU0000702111',
               'APU0000706111', 'APU0000711211', 'APU0000701312', 'APU0000717311', 'APU0000703111', 'APU0000702421',
               'APU0000711311', 'APU0000712311', 'CUUR0000SA0']
 
-# Series name corresponding the each series ID
+# Series name corresponding to each series ID
 bls_series_name = ['eggs', 'electricity', 'milk', 'bread', 'bacon', 'chicken_breast', 'chicken_whole', 'bananas',
                    'rice', 'coffee', 'ground_chuck', 'cookies', 'oranges', 'tomatoes', 'cpi_values']
 
