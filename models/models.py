69cd22abb3fd2bcb273d8d59f5252986b20d3cc6
thecodingmango <120285572+thecodingmango@users.noreply.github.com>
2025-02-22 20:49:59 -0500

    - Added linear regression model , Seasonal Naives, XGBoost, SARIMA, SARIMA + XGBoost for uni-variate forecasting

--- models/models.py
+++ models/models.py
@@ -323,36 +323,36 @@ class Model:
 #test_lag = model.lag_features(data,[1, 3, 6, 12], True)
 #model.model_building()
 
-
-dataset = [
-    './data/bls_food.csv',
-    './data/bls_gas_price.csv',
-    './data/eia_crude_price.csv'
-]
-
-sarima_order = {
-    'Cpi Values' : [(7, 2, 0), (2, 0, 0, 12)],
-    'PPI Values': [(3, 1, 2), (2, 0, 0, 12)],
-    'Unemployment': [(0,1,0), (0, 0, 0, 0)],
-    'Unleaded Gasoline': [(0, 1, 1), (0, 0, 0, 0)],
-    'UK.Brent Prices': [(1, 1, 0), (0, 0, 0, 0)],
-    'WTI Prices': [(1, 1, 0), (0, 0, 0, 0)]
-}
-
-result = pd.DataFrame()
-result['year_month'] = []
-
-
-for path in dataset:
-    data = pd.read_csv(path)
-    for column in data.columns[1:-1]:
-        if column in sarima_order:
-            model = Model(data, y=column, order=sarima_order[column][0],
-                          seasonal_order=sarima_order[column][1])
-            output = model.model_building()
-            result = result.merge(output, how='outer', on='year_month')
-
-result.to_csv('./data/forecast_data.csv')
+# Commented out to conserve computational power
+# dataset = [
+#     './data/bls_food.csv',
+#     './data/bls_gas_price.csv',
+#     './data/eia_crude_price.csv'
+# ]
+#
+# sarima_order = {
+#     'Cpi Values' : [(7, 2, 0), (2, 0, 0, 12)],
+#     'PPI Values': [(3, 1, 2), (2, 0, 0, 12)],
+#     'Unemployment': [(0,1,0), (0, 0, 0, 0)],
+#     'Unleaded Gasoline': [(0, 1, 1), (0, 0, 0, 0)],
+#     'UK.Brent Prices': [(1, 1, 0), (0, 0, 0, 0)],
+#     'WTI Prices': [(1, 1, 0), (0, 0, 0, 0)]
+# }
+#
+# result = pd.DataFrame()
+# result['year_month'] = []
+#
+#
+# for path in dataset:
+#     data = pd.read_csv(path)
+#     for column in data.columns[1:-1]:
+#         if column in sarima_order:
+#             model = Model(data, y=column, order=sarima_order[column][0],
+#                           seasonal_order=sarima_order[column][1])
+#             output = model.model_building()
+#             result = result.merge(output, how='outer', on='year_month')
+#
+# result.to_csv('./data/forecast_data.csv')
 
 
 
